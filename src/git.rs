0d8d7bd6b4e5df562c1e021eb5dc2987333bd87d
Amund Tenstad <github@amund.io>
2022-11-28 15:21:36 +0100

    Implement diff-store

--- src/git.rs
+++ src/git.rs
@@ -1,86 +1,127 @@
 use std::{
-    fs, io,
-    path::Path,
-    thread,
+    env, fs, io,
+    path::{Path, PathBuf},
     time::{Duration, Instant},
 };
 
-use git2::Repository;
+use actix_web::web;
+use git2::{Commit, Repository, Signature};
+use lazy_static::lazy_static;
 
 use crate::{
     error::Error,
-    metrics::{FILE_READ_TIME, REPO_CHEKOUT_TIME, REPO_FETCH_TIME},
-    GIT_REPO_URL, REPO_LOCK,
+    metrics::{REPO_CHEKOUT_TIME, REPO_COMMIT_TIME, REPO_FETCH_TIME, REPO_PUSH_TIME},
 };
 
-pub struct ReusableRepo {}
+lazy_static! {
+    pub static ref GIT_REPOS_ROOT_PATH: String =
+        env::var("GIT_REPOS_ROOT_PATH").unwrap_or_else(|e| {
+            tracing::error!(
+                error = e.to_string().as_str(),
+                "GIT_REPOS_ROOT_PATH not found"
+            );
+            std::process::exit(1)
+        });
+    static ref GIT_REPO_URL: String = env::var("GIT_REPO_URL").unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string().as_str(), "GIT_REPO_URL not found");
+        std::process::exit(1)
+    });
+}
 
-impl ReusableRepo {
-    pub fn get_pool() -> Result<Vec<Repository>, Error> {
-        (0..32)
-            .map(|i| Ok(Repository::open(&format!("/repo/{}", i))?))
-            .collect()
-    }
+pub struct ReusableRepoPool {
+    repos: Vec<Repository>,
+}
 
-    pub fn create_pool() -> Result<(), Error> {
-        let path = "/repo/0";
-        Repository::open(&path).or_else(|_| Repository::clone(&GIT_REPO_URL.clone(), &path))?;
-        for i in 1..32 {
-            copy_dir_all("/repo/0", format!("/repo/{}", i))?;
+impl ReusableRepoPool {
+    /// Create a pool of repos, each in a subfolder of the given path.
+    pub fn new(root_path: String, size: u64) -> Result<Self, Error> {
+        let path = format!("{}/0", root_path);
+        // Repo might already exist (persistent volume)
+        Repository::open(&path).or_else(|_| Repository::clone(&GIT_REPO_URL, &path))?;
+
+        // Create n copies of the same repo, no need to clone n-1 more times.
+        for i in 1..size {
+            let destination = format!("{}/{}", root_path, i);
+            if !Path::exists(destination.as_ref()) {
+                copy_dir_recursive(&path, destination)?;
+            }
         }
-        Ok(())
+
+        let repos = (0..size)
+            .map(|i| {
+                let path = format!("{}/{}", root_path, i);
+                Ok(Repository::open(&path)?)
+            })
+            .collect::<Result<_, git2::Error>>()?;
+
+        Ok(Self { repos })
     }
 
-    async fn new() -> Repository {
+    /// Get an available repository from pool. Must call push(repo) to put it back when done.
+    pub async fn pop(pool: &web::Data<async_lock::Mutex<ReusableRepoPool>>) -> Repository {
         loop {
-            let mut repos = REPO_LOCK.lock().await;
-            if let Some(repo) = repos.pop() {
+            if let Some(repo) = pool.lock().await.repos.pop() {
                 return repo;
             }
-            drop(repos);
-            thread::sleep(Duration::from_millis(5));
+            async_std::task::sleep(Duration::from_millis(10)).await;
         }
     }
 
-    async fn recycle(repo: Repository) {
-        let mut indexes = REPO_LOCK.lock().await;
-        indexes.push(repo);
+    /// Put a repo back in pool.
+    pub async fn push(pool: &web::Data<async_lock::Mutex<ReusableRepoPool>>, repo: Repository) {
+        pool.lock().await.repos.push(repo);
     }
 }
 
-fn sync_repo(repo: &Repository) -> Result<bool, Error> {
+/// Checkout main branch and fetch updates.
+pub fn checkout_main_and_fetch_updates(repo: &Repository) -> Result<bool, Error> {
     let start_time = Instant::now();
 
-    repo.find_remote("origin")?.fetch(&["master"], None, None)?;
-
-    let fetch_head = repo.find_reference("FETCH_HEAD")?;
-    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
-    let analysis = repo.merge_analysis(&[&fetch_commit])?;
-
-    let result = if analysis.0.is_up_to_date() {
-        Ok(false)
-    } else if analysis.0.is_fast_forward() {
-        let refname = format!("refs/heads/master");
-        let mut reference = repo.find_reference(&refname)?;
-        reference.set_target(fetch_commit.id(), "Fast-Forward")?;
-        repo.set_head(&refname)?;
-        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
-        Ok(true)
-    } else {
-        Err("Not able to fast-forward repo.".into())
-    };
-
-    let elapsed_millis = start_time.elapsed().as_millis();
-    if let Ok(change) = result {
-        REPO_FETCH_TIME
-            .with_label_values(&[&format!("{}", change)])
-            .observe(elapsed_millis as f64 / 1000.0);
+    let refname = "refs/heads/main";
+    if let Err(e) = repo.head() {
+        // Default ref is master, swap to main.
+        if e.message() == "reference 'refs/heads/master' not found" {
+            repo.set_head(refname)?;
+        }
     }
 
-    result
+    repo.find_remote("origin")?.fetch(&["main"], None, None)?;
+
+    let updated = if let Ok(fetch_head) = repo.find_reference("FETCH_HEAD") {
+        let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
+        let analysis = repo.merge_analysis(&[&fetch_commit])?;
+
+        if analysis.0.is_up_to_date() {
+            Ok(false)
+        } else if analysis.0.is_fast_forward() {
+            repo.find_reference(refname)
+                .and_then(|mut reference| {
+                    reference.set_target(fetch_commit.id(), "Fast-Forward")?;
+                    Ok(reference)
+                })
+                // Reference might not exist when cloned repo is empty.
+                .or_else(|_| repo.reference(refname, fetch_commit.id(), true, ""))?;
+
+            repo.set_head(refname)?;
+            repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
+            Ok(true)
+        } else {
+            Err(Error::from("Not able to fast-forward repo."))
+        }
+    } else {
+        Ok(false)
+    }?;
+
+    let elapsed_millis = start_time.elapsed().as_millis();
+    REPO_FETCH_TIME
+        .with_label_values(&[&format!("{}", updated)])
+        .observe(elapsed_millis as f64 / 1000.0);
+
+    Ok(updated)
 }
 
-fn checkout_timestamp(repo: &Repository, timestamp: u64) -> Result<bool, Error> {
+/// Checkout a timestamp. Returns false if no files exists at that point in time.
+pub fn checkout_timestamp(repo: &Repository, timestamp: u64) -> Result<bool, Error> {
     let start_time = Instant::now();
 
     let mut revwalk = repo.revwalk()?;
@@ -114,9 +155,8 @@ fn checkout_timestamp(repo: &Repository, timestamp: u64) -> Result<bool, Error> 
             }
 
             let refname = format!("refs/heads/{}", &oid.to_string());
-            let obj = repo.revparse_single(&refname)?;
-            repo.checkout_tree(&obj, None)?;
             repo.set_head(&refname)?;
+            repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
             Ok(true)
         }
     };
@@ -131,51 +171,66 @@ fn checkout_timestamp(repo: &Repository, timestamp: u64) -> Result<bool, Error> 
     result
 }
 
-pub async fn fetch_graphs(timestamp: u64) -> Result<Vec<Vec<u8>>, Error> {
-    let repo = ReusableRepo::new().await;
-    sync_repo(&repo)?;
-    match checkout_timestamp(&repo, timestamp)? {
-        true => {
-            let repo_dir = repo.path().parent().ok_or::<Error>("Invalid repo".into())?;
-            let files = read_files(repo_dir).await?;
-            ReusableRepo::recycle(repo).await;
-
-            Ok(files)
-        }
-        false => {
-            ReusableRepo::recycle(repo).await;
-
-            Ok(Vec::new())
-        }
-    }
-}
-
-async fn read_files(path: &Path) -> Result<Vec<Vec<u8>>, Error> {
+/// Commit file.
+pub async fn commit_file(repo: &Repository, path: &PathBuf, message: String) -> Result<(), Error> {
     let start_time = Instant::now();
 
-    let mut files = Vec::new();
-    for filepath in path.read_dir()? {
-        let filepath = filepath?;
-        if filepath.file_type()?.is_file() {
-            files.push(tokio::fs::read(filepath.path()).await?)
-        }
+    let mut index = repo.index()?;
+    index.add_path(path)?;
+    index.write()?;
+
+    let tree_id = index.write_tree()?;
+    let tree = repo.find_tree(tree_id)?;
+
+    let mut parents = Vec::new();
+    // repo.head() fails when empty repo is cloned and no commits exist. In that case, there is no parents.
+    if let Ok(Some(parent)) = repo.head().map(|r| r.target()) {
+        parents.push(repo.find_commit(parent)?);
     }
 
-    let elapsed_millis = start_time.elapsed().as_millis();
-    FILE_READ_TIME.observe(elapsed_millis as f64 / 1000.0);
+    let signature = Signature::now("rdf-diff-store", "fellesdatakatalog@digdir.no")?;
+    repo.commit(
+        Some("HEAD"),
+        &signature,
+        &signature,
+        message.as_str(),
+        &tree,
+        parents.iter().collect::<Vec<&Commit>>().as_slice(),
+    )?;
 
-    Ok(files)
+    let elapsed_millis = start_time.elapsed().as_millis();
+    REPO_COMMIT_TIME.observe(elapsed_millis as f64 / 1000.0);
+
+    Ok(())
 }
 
-fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src)? {
+/// Push commits.
+pub fn push_updates(repo: &Repository) -> Result<(), Error> {
+    let start_time = Instant::now();
+
+    repo.find_remote("origin")?
+        .push(&["refs/heads/main:refs/heads/main"], None)?;
+
+    let elapsed_millis = start_time.elapsed().as_millis();
+    REPO_PUSH_TIME.observe(elapsed_millis as f64 / 1000.0);
+
+    Ok(())
+}
+
+/// Copy folder recursively.
+pub fn copy_dir_recursive(
+    source: impl AsRef<Path>,
+    destination: impl AsRef<Path>,
+) -> io::Result<()> {
+    fs::create_dir_all(&destination)?;
+    for entry in fs::read_dir(source)? {
         let entry = entry?;
-        let ty = entry.file_type()?;
-        if ty.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        let file_type = entry.file_type()?;
+        let target = destination.as_ref().join(entry.file_name());
+        if file_type.is_dir() {
+            copy_dir_recursive(entry.path(), target)?;
         } else {
-            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+            fs::copy(entry.path(), target)?;
         }
     }
     Ok(())
